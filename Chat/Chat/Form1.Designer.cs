00901cc9749a1c641a9aac7050585e4c1e7f4e61
Jorge Estrada <co.jestrada@gmail.com>
2016-03-06 15:14:02 -0500

    Se construy el formulario principal y ya funciona el servidor para escuchar mensajes

--- Chat/Chat/Form1.Designer.cs
+++ Chat/Chat/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Chat
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConversacion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP No";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(53, 9);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 20);
+            this.txtIP.TabIndex = 1;
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(53, 35);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(36, 20);
+            this.txtPuerto.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Puerto";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(93, 34);
+            this.btnConectar.Margin = new System.Windows.Forms.Padding(1);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(60, 23);
+            this.btnConectar.TabIndex = 4;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(53, 62);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(100, 20);
+            this.txtNick.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nick";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(15, 477);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(394, 44);
+            this.txtMensaje.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 461);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mensaje";
+            // 
+            // txtConversacion
+            // 
+            this.txtConversacion.Location = new System.Drawing.Point(15, 113);
+            this.txtConversacion.Multiline = true;
+            this.txtConversacion.Name = "txtConversacion";
+            this.txtConversacion.Size = new System.Drawing.Size(462, 345);
+            this.txtConversacion.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Conversación";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(419, 475);
+            this.btnEnviar.Margin = new System.Windows.Forms.Padding(1);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(60, 46);
+            this.btnEnviar.TabIndex = 11;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(489, 533);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtConversacion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Chat";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConversacion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEnviar;
     }
 }
 
--- Chat/Chat/Form1.cs
+++ Chat/Chat/Form1.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading; // para usar hilos en mi aplicación
 
 namespace Chat
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        // tcplistener objeto para hacer escucha a través de un socket
+        private TcpListener servidor;
+        // conexion con el cliente
+        private TcpClient cliente;
+        // definir un flujo de bytes
+        Byte[] bytesCliente;
+        // definir un stream de bytes que viene desde el cliente
+        NetworkStream streamCliente;
+        // cadena de caracteres
+        string mensaje;
+
+        public frmMain()
         {
             InitializeComponent();
         }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            // Thread es una clase que permite definir hilos
+            Thread tarea = new Thread(hilo);
+            // Comienza el hilo
+            tarea.Start();
+        }
+
+        private void hilo()
+        {
+            try
+            {
+                // instanciamos un TcpListener
+                servidor = new TcpListener(IPAddress.Parse(txtIP.Text), int.Parse(txtPuerto.Text));
+                // empieza la escucha
+                servidor.Start();
+                // ciclo infinito de escucha
+                while (true)
+                {
+                    cliente = servidor.AcceptTcpClient();
+                    bytesCliente = new byte[256];
+                    streamCliente = cliente.GetStream();
+                    // aquí hemos colocado en la variable bytesCliente el 
+                    // flujo de datos que viene del cliente
+                    streamCliente.Read(bytesCliente, 0, bytesCliente.Length);
+                    // convertimos el clujo de bytes en cadena de texto
+                    mensaje = Encoding.ASCII.GetString(bytesCliente, 0, bytesCliente.Length);
+                    // invocando al hilo principal
+                    txtConversacion.Invoke(new EventHandler(ImprimirMensaje));
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void ImprimirMensaje(object sender, EventArgs e)
+        {
+            txtConversacion.Text = mensaje;
+        }
+
     }
 }
